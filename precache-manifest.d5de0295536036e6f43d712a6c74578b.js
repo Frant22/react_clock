385f279640534c53f61287d38f0a425ee6303177
Vladyslav Semyrekov <semyrekov.vladyslav@gmail.com>
2021-03-22 10:23:10 +0200

    Updates

---
+++ precache-manifest.d5de0295536036e6f43d712a6c74578b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d940ad107ad60d98aa559d3d25185e9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4f2663f0f48b303c5fd5",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f2663f0f48b303c5fd5",
+    "url": "./static/js/main.12754055.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
